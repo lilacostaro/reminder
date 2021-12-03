9a20a6d8d509f068442b35cec25ce0bb71b1af72
lilacostaro <85863159+lilacostaro@users.noreply.github.com>
2021-12-03 01:23:31 -0300

    update settings

--- reminder/settings.py
+++ reminder/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-73nc$d^x5*h$g5-n5rn_r@)=dp^%_m#ou9j_1=&h0!a2cng7s8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
